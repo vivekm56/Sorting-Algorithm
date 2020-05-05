4c23002d393c30a5668775e0cc1ff6736f039321
vivek_mudgal <29491716+vivekm56@users.noreply.github.com>
2020-05-05 22:45:42 +0530

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -7,8 +7,11 @@ for i in range(num):
   arr.append(n)
 print("Arr",arr) 
 #Concept of bubbleSort starts here 
+c=0
 for i in range(len(arr)-1):#Take the length of array for sat the number of steps for comparison
         for j in range(0, len(arr)-i-1):#Here we sat number of swap
             if arr[j] > arr[j+1]:# compare the values of array
                 arr[j], arr[j+1] = arr[j+1], arr[j]#swap or manage array's values in order
+                c+=1
+print("swapCount = ",c)#count number of swap
 print("Sorted Arr",arr)#Print sorted array 
